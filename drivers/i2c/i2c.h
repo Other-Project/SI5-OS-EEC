4bbcc57423f494c6aee340c6277c2e29c7524b2c
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2025-12-23 18:38:26 +0100

    refactor: Higher level of data in I2C

--- drivers/i2c/i2c.h
+++ drivers/i2c/i2c.h
@@ -6,25 +6,6 @@
 // Definition of protocol registers
 #define I2C_NUM_REGISTERS 32
 
-// Register map (customize as needed)
-#define REG_STATUS          0x00  // General system status
-#define REG_ALARM_STATE     0x01  // Alarm state (0=off, 1=on)
-#define REG_MOTION_DETECTED 0x02  // Motion detected (0=no, 1=yes)
-#define REG_BUZZER_CMD      0x03  // Buzzer command (0=off, 1=on)
-#define REG_LED_CMD         0x04  // LED command (0=off, 1=on)
-#define REG_DISTANCE_H      0x05  // Ultrasonic sensor distance (MSB)
-#define REG_DISTANCE_L      0x06  // Ultrasonic sensor distance (LSB)
-#define REG_RFID_STATUS     0x07  // RFID status (0=no tag, 1=tag present)
-#define REG_RFID_ID_0       0x08  // RFID ID byte 0
-#define REG_RFID_ID_1       0x09  // RFID ID byte 1
-#define REG_RFID_ID_2       0x0A  // RFID ID byte 2
-#define REG_RFID_ID_3       0x0B  // RFID ID byte 3
-#define REG_RFID_ID_4       0x0C  // RFID ID byte 4
-#define REG_RFID_ID_5       0x0D  // RFID ID byte 5
-#define REG_ROTARY_ANGLE    0x10  // Potentiometer angle
-#define REG_BUTTON_STATE    0x11  // Button state (0=released, 1=pressed)
-#define REG_COMMAND         0x12  // General command register
-#define REG_ERROR_CODE      0x13  // Error code
 
 // Callback type for register changes
 typedef void (*I2CCallback)(uint8_t reg, uint8_t value);
--- main.cpp
+++ main.cpp
@@ -9,203 +9,139 @@
 #include "drivers/button/button.h"
 #include "drivers/rotary_angle/rotary_angle.h"
 #include "drivers/i2c/i2c.h"
+#include "consts.h"
 
 // Tasks
-static void vReadRfid(void *pvParameters);
-static void vBuzzerTask(void *pvParameters);
 static void vUltrasonicTask(void *pvParameters);
-static void vRotaryAngleTask(void *pvParameters);
-static void vI2CUpdateTask(void *pvParameters);
+static void vReadRfid(void *pvParameters);
+static void vButtonTask(void *pvParameters);
 
 // Peripherals
 static RFID_Reader rfid(7, 8);
-static Buzzer grooveBuzzer(&DDRD, &PORTD, _BV(PD6));
+static Buzzer buzzer(&DDRD, &PORTD, _BV(PD6));
 static Buzzer led(&DDRD, &PORTD, _BV(PD5));
-static Button myButton(2);
+static Button button(2);
 static RotaryAngle rotaryAngle(0);
-static uint8_t buffer[16];
 
-// I2C callbacks to react to commands from the Raspberry Pi
-void onBuzzerCommand(uint8_t reg, uint8_t value) {
-    if (value) {
-        grooveBuzzer.on();
-    } else {
-        grooveBuzzer.off();
-    }
+void setEventFlag(uint8_t event, bool enable)
+{
+    uint8_t currentEvents = I2C_Protocol::getRegister(REG_EVENTS);
+    if (enable)
+        currentEvents |= event;
+    else
+        currentEvents &= ~event;
+    I2C_Protocol::setRegister(REG_EVENTS, currentEvents);
 }
 
-void onLedCommand(uint8_t reg, uint8_t value) {
-    if (value) {
-        led.on();
-    } else {
-        led.off();
-    }
-}
-
-void onAlarmCommand(uint8_t reg, uint8_t value) {
-    // Update alarm state
-    I2C_Protocol::setRegister(REG_ALARM_STATE, value);
-}
-
-void onI2CCommand(uint8_t reg, uint8_t value) {
-    switch (reg)
+void onStatusChange(uint8_t newStatus)
+{
+    switch (newStatus)
     {
-    case REG_BUZZER_CMD:
-        onBuzzerCommand(reg, value);
+    case STATUS_DISARMED:
+        led.off();
+        buzzer.off();
         break;
-    case REG_LED_CMD:
-        onLedCommand(reg, value);
+    case STATUS_ARMED:
+        led.on();
+        buzzer.off();
         break;
-    case REG_ALARM_STATE:
-        onAlarmCommand(reg, value);
+    case STATUS_TRIGGERED:
+        // TODO: Led blinking
+        buzzer.on();
         break;
     default:
         break;
     }
 }
 
-int main(void) {
+void onI2CCommand(uint8_t reg, uint8_t value)
+{
+    switch (reg)
+    {
+    case REG_STATUS:
+        onStatusChange(value);
+        break;
+    default:
+        break;
+    }
+}
+
+int main(void)
+{
     // Initialize Wire (required before I2C_Protocol)
     Wire.begin();
-    
+
     // Initialize I2C in slave mode (address 0x32)
     I2C_Protocol::init(0x32);
-    
+
     // Register callbacks for commands coming from the Raspberry Pi
     I2C_Protocol::registerCallback(onI2CCommand);
-    
+
     // Initialize peripherals
     led.init();
-    grooveBuzzer.init();
-    myButton.init();
+    buzzer.init();
+    button.init();
     rfid.begin(9600);
     rotaryAngle.init();
-    
+
     // Create tasks
-    //xTaskCreate(vReadRfid, "rfid", configMINIMAL_STACK_SIZE + 50, NULL, 2U, NULL);
-    //xTaskCreate(vBuzzerTask, "buzzer", configMINIMAL_STACK_SIZE, NULL, 1U, NULL);
     xTaskCreate(vUltrasonicTask, "ultrasonic", configMINIMAL_STACK_SIZE, NULL, 1U, NULL);
-    //xTaskCreate(vRotaryAngleTask, "rotary", configMINIMAL_STACK_SIZE, NULL, 1U, NULL);
-    //xTaskCreate(vI2CUpdateTask, "i2c_update", configMINIMAL_STACK_SIZE, NULL, 1U, NULL);
-    
+    xTaskCreate(vReadRfid, "rfid", configMINIMAL_STACK_SIZE, NULL, 1U, NULL);
+    xTaskCreate(vButtonTask, "button", configMINIMAL_STACK_SIZE, NULL, 1U, NULL);
+
     // Start scheduler
     vTaskStartScheduler();
-    
+
     return 0;
 }
 
-// RFID task - reads tags and updates I2C registers
-static void vReadRfid(void *pvParameters) {
+static void vUltrasonicTask(void *pvParameters)
+{
+    Ultrasonic ultrasonic(&PORTD, &DDRD, &PIND, PD4);
     TickType_t xLastWakeUpTime = xTaskGetTickCount();
-    size_t length;
-    
-    while (1) {
-        if (rfid.dataAvailable()) {
-            buffer[0] = '\0';
-            length = rfid.readData(buffer, sizeof(buffer) - 1);
-            
-            if (length >= 10) {
-                // Tag detected
-                I2C_Protocol::setRegister(REG_RFID_STATUS, 1);
-                
-                // Copy the tag ID into the registers (max 8 bytes)
-                for (uint8_t i = 0; i < 8 && (i + 1) < length; i++) {
-                    I2C_Protocol::setRegister(REG_RFID_ID_0 + i, buffer[i + 1]);
+
+    while (1)
+    {
+        uint16_t distance_mm = ultrasonic.MeasureInMillimeters();
+        setEventFlag(EVENT_MOTION_DETECTED, distance_mm > 1000);
+        vTaskDelayUntil(&xLastWakeUpTime, 200 / portTICK_PERIOD_MS);
+    }
+}
+
+static void vReadRfid(void *pvParameters)
+{
+    TickType_t xLastWakeUpTime = xTaskGetTickCount();
+    while (1)
+    {
+        setEventFlag(EVENT_RFID_READ, false);
+        if (rfid.dataAvailable())
+        {
+            uint64_t cardNumber = rfid.readCardNumber(); // 48 bits card number
+            if (cardNumber != 0)
+            {
+                // Store card number in I2C register
+                for (int i = 0; i < 6; i++)
+                {
+                    uint8_t byteValue = (cardNumber >> (8 * (5 - i))) & 0xFF;
+                    I2C_Protocol::setRegister(REG_RFID + i, byteValue);
                 }
-                
-                vTaskDelayUntil(&xLastWakeUpTime, 2000 / portTICK_PERIOD_MS);
+
+                // Set RFID read event
+                setEventFlag(EVENT_RFID_READ, true);
             }
-        } else {
-            // No tag
-            I2C_Protocol::setRegister(REG_RFID_STATUS, 0);
         }
-        
         vTaskDelayUntil(&xLastWakeUpTime, 100 / portTICK_PERIOD_MS);
     }
 }
 
-// Ultrasonic task - measures distance and detects motion
-static void vUltrasonicTask(void *pvParameters) {
-    Ultrasonic ultrasonic(&PORTD, &DDRD, &PIND, PD4);
+static void vButtonTask(void *pvParameters)
+{
     TickType_t xLastWakeUpTime = xTaskGetTickCount();
-    
-    while (1) {
-        long distance_mm = ultrasonic.MeasureInMillimeters();
-        
-        // Store distance in 2 registers (16 bits)
-        I2C_Protocol::setRegister(REG_DISTANCE_H, (distance_mm >> 8) & 0xFF);
-        I2C_Protocol::setRegister(REG_DISTANCE_L, distance_mm & 0xFF);
-        
-        // Motion detection (distance < 1000mm)
-        if (distance_mm < 1000 && distance_mm > 0) {
-            I2C_Protocol::setRegister(REG_MOTION_DETECTED, 1);
-        } else {
-            I2C_Protocol::setRegister(REG_MOTION_DETECTED, 0);
-        }
-        
-        vTaskDelayUntil(&xLastWakeUpTime, 200 / portTICK_PERIOD_MS);
-    }
-}
-
-// Button task - toggles the alarm
-static void vBuzzerTask(void *pvParameters) {
-    while (1) {
-        // Wait for the button press
-        myButton.waitForPress();
-        
-        // Toggle alarm state
-        uint8_t current_state = I2C_Protocol::getRegister(REG_ALARM_STATE);
-        I2C_Protocol::setRegister(REG_ALARM_STATE, !current_state);
-        
-        // Confirmation beep
-        grooveBuzzer.on();
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-        grooveBuzzer.off();
-    }
-}
-
-// Potentiometer task - reads the angle
-static void vRotaryAngleTask(void *pvParameters) {
-    TickType_t xLastWakeUpTime = xTaskGetTickCount();
-    
-    while (1) {
-        long angle = rotaryAngle.readDegrees();
-        
-        // Store the angle in a register (0-255 for 0-300°)
-        uint8_t angle_byte = (angle * 255) / 300;
-        I2C_Protocol::setRegister(REG_ROTARY_ANGLE, angle_byte);
-        
-        vTaskDelayUntil(&xLastWakeUpTime, 200 / portTICK_PERIOD_MS);
-    }
-}
-
-// I2C update task - handles alarm logic
-static void vI2CUpdateTask(void *pvParameters) {
-    TickType_t xLastWakeUpTime = xTaskGetTickCount();
-    
-    while (1) {
-        // Read alarm state
-        uint8_t alarm_state = I2C_Protocol::getRegister(REG_ALARM_STATE);
-        uint8_t motion = I2C_Protocol::getRegister(REG_MOTION_DETECTED);
-        
-        // If alarm enabled AND motion detected -> trigger buzzer
-        if (alarm_state && motion) {
-            I2C_Protocol::setRegister(REG_BUZZER_CMD, 1);
-            grooveBuzzer.on();
-        } else {
-            I2C_Protocol::setRegister(REG_BUZZER_CMD, 0);
-            grooveBuzzer.off();
-        }
-        
-        // Update status LED
-        if (alarm_state) {
-            led.on();
-        } else {
-            led.off();
-        }
-        
-        I2C_Protocol::setRegister(REG_STATUS, 0x01); // System OK
-        
-        vTaskDelayUntil(&xLastWakeUpTime, 50 / portTICK_PERIOD_MS);
+    while (1)
+    {
+        setEventFlag(EVENT_BTN_PRESSED, false);
+        if (button.waitForPress())
+            setEventFlag(EVENT_BTN_PRESSED, true);
+        vTaskDelayUntil(&xLastWakeUpTime, 100 / portTICK_PERIOD_MS);
     }
 }
