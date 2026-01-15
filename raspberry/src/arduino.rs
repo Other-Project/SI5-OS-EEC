de2fc3ce49735c4b5174769d3122d241db95efbd
EvanG <evan_g@orange.fr>
2026-01-15 17:52:41 +0100

    refactor(rasp): Add a mutex on I2C bus

--- raspberry/src/arduino.rs
+++ raspberry/src/arduino.rs
@@ -1,23 +1,25 @@
 use anyhow::Result;
 use rppal::i2c::I2c;
+use std::sync::Mutex;
 
 use crate::arduino_consts::{Events, Register, SecurityState};
 const READ_FLAG: u8 = 0x80;
 
 pub struct ArduinoI2C {
-    bus: I2c,
+    bus: Mutex<I2c>,
 }
 
 impl ArduinoI2C {
     pub fn new(i2c_slave_addr: u16) -> Result<Self> {
         let mut bus = I2c::new()?;
         bus.set_slave_address(i2c_slave_addr)?;
-        Ok(Self { bus })
+        Ok(Self { bus: Mutex::new(bus) })
     }
 
     fn write_register(&mut self, reg: Register, value: u8) -> Result<()> {
         let buf = [reg as u8, value];
-        self.bus.write(&buf)?;
+        let mut bus = self.bus.lock().unwrap();
+        bus.write(&buf)?;
         Ok(())
     }
 
@@ -30,10 +32,11 @@ impl ArduinoI2C {
     }
 
     fn read_registers(&mut self, start_reg: Register, count: u8) -> Result<Vec<u8>> {
-        self.bus.write(&[start_reg as u8 | READ_FLAG, count])?; // Request read
+        let mut bus = self.bus.lock().unwrap();
+        bus.write(&[start_reg as u8 | READ_FLAG, count])?; // Request read
 
         let mut buf = vec![0u8; count as usize + 1]; // +1 for checksum
-        self.bus.read(&mut buf)?;
+        bus.read(&mut buf)?;
 
         let (values, checksum_byte) = buf.split_at(count as usize);
         let received_checksum = checksum_byte[0];
@@ -41,10 +44,11 @@ impl ArduinoI2C {
 
         if computed_checksum != received_checksum {
             Err(anyhow::anyhow!(
-                "Checksum mismatch for register {:?}: computed {}, received {}",
+                "Checksum mismatch for register {:?}: computed {}, received {}, values: {:?}",
                 start_reg,
                 computed_checksum,
-                received_checksum
+                received_checksum,
+                values
             ))
         } else {
             Ok(values.to_vec())
